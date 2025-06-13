215458e52182eb1f19d23588375dbc025bd2ab5c
Riccardo Maniello <60267238+RiccardoManiello@users.noreply.github.com>
2025-06-13 12:10:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -32,8 +32,8 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function disegnaGriglia() {
-    const cols = 5;
-    const rows = 5;
+    const cols = 20;
+    const rows = 20;
     const cellW = canvas.width / cols;
     const cellH = canvas.height / rows;
 
